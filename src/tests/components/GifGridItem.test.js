fcd1026228258f6c0a0a55b32d3946b06d6b2072
fredycorts7 <fred.cor.14@gmail.com>
2020-07-02 20:49:25 -0500

    Terminando de probar el componente GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,11 +4,35 @@ import GifGridItem from '../../components/GifGridItem';
 import '@testing-library/jest-dom';
 
 describe('Testing to <GifGridItem/>', () => {
-  test('should be the same component', () => {
-    const title = 'Naruto';
-    const url = 'naruto.jpg';
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
+  const title = 'Naruto';
+  const url = 'naruto.jpg';
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
+  test('should be the same component', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should has a paragraph with the title', () => {
+    const paragraph = wrapper.find('p');
+
+    expect(paragraph.text().trim()).toBe(title);
+  });
+
+  test('should has the image equal to url and alt of props', () => {
+    const img = wrapper.find('img');
+    const { src, alt } = img.props();
+
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test('should has the same class', () => {
+    const classNameExpected = 'animate__fadeIn';
+    const div = wrapper.find('div');
+    // const [, , classNameTest] = div.prop('className').split(' ');
+    const classNameTest = div.prop('className').includes(classNameExpected);
+
+    // expect(classNameTest).toBe(classNameExpected);
+    expect(classNameTest).toBeTruthy();
   });
 });
